84125d84f50e3295b2c505ad3466ccb8ad4c7fd1
03y <98747393+03y@users.noreply.github.com>
2022-09-22 17:33:04 +0100

    Test cases for views

--- onlyProfits_app/tests/test_urls.py
+++ onlyProfits_app/tests/test_urls.py
@@ -1,29 +1,39 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from onlyProfits_app.views import IndexView
+from onlyProfits_app.views import AccountView, CreateAccountView, IndexView, MarketsView, SavedMarketsView, SpecificMarketView
+from onlyProfits_app.models import Market
+from django.contrib.auth.models import User
 
 """
 Using SimpleTestCase rather than TestCase as do not need to interact with database
 """
 
-
 class TestUrls(SimpleTestCase):
-
+    
     def test_index_url_resolves(self):
         url = reverse('onlyProfits_app:index')
         self.assertEquals(resolve(url).func.view_class, IndexView)
 
+    def test_create_account_url_resolves(self):
+        url = reverse('onlyProfits_app:create_account')
+        self.assertEquals(resolve(url).func.view_class, CreateAccountView)
+    
     def test_account_url_resolves(self):
-        pass
+        account = User.objects.create()
+        url = reverse('onlyProfits_app:account', kwargs=account.username)
+        self.assertEquals(resolve(url).func.view_class, AccountView)
 
     def test_markets_url_resolves(self):
-        pass
-
-    def test_create_new_account_url_resolves(self):
-        pass
+        url = reverse('onlyProfits_app:markets')
+        self.assertEquals(resolve(url).func.view_class, MarketsView)
 
     def test_saved_markets_url_resolves(self):
-        pass
+        account = User.objects.create()
+        url = reverse('onlyProfits_app:saved_markets', kwargs=account.username)
+        self.assertEquals(resolve(url).func.view_class, SavedMarketsView)
 
     def test_specific_market_url_resolves(self):
-        pass
+        market = Market.objects.create()
+        url = reverse('onlyProfits_app:specific_market', kwargs=market.ticker)
+        self.assertEquals(resolve(url).func.view_class, SpecificMarketView)
+    
--- onlyProfits_app/tests/test_views.py
+++ onlyProfits_app/tests/test_views.py
@@ -1,12 +1,81 @@
 from django.test import TestCase
 from django.urls import reverse
 
+# from django.contrib.auth.models import User
+from onlyProfits_app.models import Market, User
 
-class ViewTestCase(TestCase):
+class IndexViewTest(TestCase):
+    def setUpTestData(cls):
+        User.objects.create(username="test_user", password="test_password")
+        for i in range(5):
+            Market.objects.create(ticker="EXAMPLE" + str(i), values=[i, i + 3, i + 2, i + 4, i + 5])
     
     def test_check_template(self):
         self.assertTemplateUsed(self.client.get("onlyProfits:/"), "onlyProfits_app/onlyProfits.html")
     
     def test_context_dictionary(self):
-        # TODO: Write test to check context dictionary
-        pass
+        response = self.client.get(reverse("onlyProfits_app:index"))
+        expected_markets = Market.objects.order_by("volume")
+        self.assertEqual(response.context["markets"], expected_markets)
+        self.assertTrue("markets" in response.context)
+
+class CreateAccountViewTest(TestCase):
+    def test_check_template(self):
+        self.assertTemplateUsed(self.client.get("onlyProfits:/create_account"), "onlyProfits_app/create_account.html")
+    
+    def test_context_dictionary(self):
+        self.assertIsNone(self.client.get(reverse("onlyProfits_app:create_account")).context)
+
+class AccountViewTest(TestCase):
+    def setUpTestData(cls):
+        for i in range(5):
+            Market.objects.create(ticker="EXAMPLE" + str(i), values=[i, i + 3, i + 2, i + 4, i + 5])
+        User.objects.create(username="test_user", password="test_password", saved_markets=["EXAMPLE1", "EXAMPLE2"])
+    
+    def test_check_template(self):
+        self.assertTemplateUsed(self.client.get("onlyProfits:/account/test_user"), "onlyProfits_app/account.html")
+    
+    def test_context_dictionary(self):
+        response = self.client.get(reverse("onlyProfits_app:account", kwargs="test_user"))
+        expected_saved_markets = self.client.get(reverse("onlyProfits_app:saved_markets", kwargs="test_user"))
+        self.assertTrue("user" in response.context)
+        self.assertEqual(response.context["user"].saved_markets, expected_saved_markets)
+
+class MarketsViewTest(TestCase):
+    def setUpTestData(cls):
+        for i in range(5):
+            Market.objects.create(ticker="EXAMPLE" + str(i), values=[i, i + 3, i + 2, i + 4, i + 5])
+    
+    def test_check_template(self):
+        self.assertTemplateUsed(self.client.get("onlyProfits:/markets"), "onlyProfits_app/markets.html")
+    
+    def test_context_dictionary(self):
+        response = self.client.get(reverse("onlyProfits_app:markets"))
+        expected_markets_order = Market.objects.order_by("volume")
+        self.assertEqual(response.context["markets"], expected_markets_order)        
+
+class SavedMarketsViewTest(TestCase):
+    def setUpTestData(cls):
+        for i in range(3):
+            Market.objects.create(ticker="EXAMPLE" + str(i), values=[i, i + 3, i + 2, i + 4, i + 5])
+        User.objects.create(username="test_user", password="test_password", saved_markets=["EXAMPLE2", "EXAMPLE3"])
+    
+    def test_check_template(self):
+        self.assertTemplateUsed(self.client.get("onlyProfits:/test_user/saved_markets"), "onlyProfits_app/saved_markets.html")
+    
+    def test_context_dictionary(self):
+        response = self.client.get(reverse("onlyProfits_app:saved_markets", kwargs="test_user"))
+        expected_saved_markets = ["EXAMPLE2", "EXAMPLE3"]
+        self.assertEqual(response.context["saved_markets"], expected_saved_markets)
+
+class SpecficMarketViewTest(TestCase):
+    def setUpTestData(cls):
+        Market.objects.create(ticker="EXAMPLE1", values=[2, 3, 1, 6, 10])
+    
+    def test_check_template(self):
+        self.assertTemplateUsed(self.client.get("onlyProfits:/market/EXAMPLE1"), "onlyProfits_app/specific_market.html")
+    
+    def test_context_dictionary(self):
+        response = self.client.get(reverse("onlyProfits_app:specific_market", kwargs="EXAMPLE1"))
+        self.assertTrue("market" in response.context)
+        self.assertEqual(response.context["market"], Market.objects.get(ticker="EXAMPLE1"))
